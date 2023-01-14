7bea9cc8bc2c3e853214ad0e0ed745bb15d4e97e
sefaakuruu1 <sefaakuruu1@gmail.com>
2023-01-14 23:17:53 +0300

    calculator finish

---
+++ script.js
@@ -0,0 +1,102 @@
+const display = document.querySelector('.calculator-input');
+const keys = document.querySelector('.calculator-keys');
+
+let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
+
+updateDisplay();
+
+function updateDisplay() {
+    display.value = displayValue;
+}
+
+keys.addEventListener('click', function(e) {
+    const element = e.target;
+    const value = element.value;
+
+    if (!element.matches('button')) return;/*eger tıkladıgımız element button degılse calcultorun keysini ekrana yazdırmA*/ 
+
+    switch(value) {
+        //gelen value bilgisine gore farklı işlemler yaptık
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;  //asagıdakı operatorlerrın kontrols edilmemesi için break kullandık
+        case '.':
+            inputDecimal();
+            break;
+        case 'clear':
+            clear();
+            break;
+        default:
+            inputNumber(element.value);        
+    }
+    updateDisplay();
+});
+
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
+
+    if(operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return;
+    }
+
+    if (firstValue === null) {
+        firstValue = value;
+    } else if (operator) {
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        //basamak sayısını sınırlandırmamızı saglar
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator;
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+}
+
+function calculate(first, second, operator) {
+    if(operator === '+') {
+        return first + second;
+    } else if (operator === '-') {
+        return first - second;
+    } else if (operator === '*') {
+        return first * second
+    } else if (operator === '/') {
+        return first / second;
+    }
+    return second;
+}
+
+function inputNumber(num) {
+    if(waitingForSecondValue) {
+        //beklenen 2.sayı true ise girilen degeri display value da goster operatore tıkladıgımızda beklenen degeri true yaparız
+        //eger false ise onceki islemşi yap
+        displayValue = num;
+        waitingForSecondValue = false;
+    } else {
+        displayValue = displayValue === '0'? num: displayValue + num;
+    }
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+}
+
+function inputDecimal() {
+    if (!displayValue.includes('.')) {
+        /*DİSPLAY DEGERİNDE EGER . VARSA EKLEMEMEMİZİ SAGLAR*/
+        displayValue += '.';
+    }
+}
+
+function clear() {
+    //TANIMLADIGIMIZ DİSPLAY DEGERİNİ SIFIRLARIZ.
+    displayValue = '0';
+}
